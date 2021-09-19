aee297725716d2ae4b32c73558a658043ef2f9fa
Harryever <82397932+Harryever@users.noreply.github.com>
2021-09-19 14:24:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function preload() {}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+  console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results) {
+  if (results.length > 0) {
+    console.log(results);
+    nose_x = results[0].pose.nose.x - 22;
+    nose_y = results[0].pose.nose.y - 24;
+    console.log("nose x = " + results[0].pose.nose.x);
+    console.log("nose y = " + results[0].pose.nose.y);
+  }
+}
+function draw() {}
+
+function take_snapshot() {
+  save("Kohana_Filtered_Img.jpg");
+}
